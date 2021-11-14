2149dbe1fd0c243c57bf56147c22a224fb9463ae
Jovan Petrovic <jovanpetrovicq@gmail.com>
2021-11-14 10:23:08 +0100

    Finished preparing the dataset script. Writing MFCCs to json file.

--- DeepLearningAudio/2_preparing_dataset/preparing_the_dataset.py
+++ DeepLearningAudio/2_preparing_dataset/preparing_the_dataset.py
@@ -3,11 +3,11 @@ import librosa
 import math
 import json
 
-DATASET_PATH = "C:/Users/proudsourceit/Desktop/MasterRad/Datasets/fma_small_by_genre"
-JSON_PATH = "data.json"
+DATASET_PATH = "C:/Users/proudsourceit/Desktop/MasterRad/Datasets/fma_small_by_genre_wav"
+JSON_PATH = "data/data.json"
 
 SAMPLE_RATE = 22050
-DURATION = 30   # measured in seconds
+DURATION = 29   # measured in seconds
 SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
 
 def save_mfcc(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
@@ -27,7 +27,7 @@ def save_mfcc(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512, nu
         if dirpath is not dataset_path:
             # save the semantic labels
             dirpath_components = dirpath.split("/") # root/rock => ["root", "rock"]
-            semantic_label = dirpath_components[-1]
+            semantic_label = dirpath_components[-1].split("\\")[-1]
             data["mapping"].append(semantic_label)
             print("\nProcessing: {}".format(semantic_label))
             # process files for specific genre
@@ -39,13 +39,17 @@ def save_mfcc(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512, nu
                 for s in range(num_segments):
                     start_sample = num_samples_per_segment * s
                     finish_sample = start_sample + num_samples_per_segment
-                    mfcc = librosa.feature.mfcc(signal[start_sample:finish_sample], sr=sr, n_fft=n_fft, n_mfcc=n_mfcc, hop_length=hop_length)
-                    mfcc = mfcc.T
-                    # store mfcc for segment if it has expected length
-                    if len(mfcc) == expected_num_mfcc_vectors_per_segment:
-                        data["mfcc"].append(mfcc.tolist())
-                        data["labels"].append(i-1)
-                        print("{}, segment:{}".format(file_path, s + 1))
+                    try:
+                        mfcc = librosa.feature.mfcc(signal[start_sample:finish_sample], sr=sr, n_fft=n_fft, n_mfcc=n_mfcc, hop_length=hop_length)
+                        mfcc = mfcc.T
+                        # store mfcc for segment if it has expected length
+                        if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+                            data["mfcc"].append(mfcc.tolist())
+                            data["labels"].append(i-1)
+                            print("{}, segment:{}".format(file_path, s + 1))
+                    except:
+                        print("ERROR! {}, segment:{}".format(file_path, s + 1))
+                        break
 
     # save MFCCs to json file
     with open(json_path, "w") as fp:
